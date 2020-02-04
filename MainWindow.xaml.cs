413d085ea123dcc2a2a6fa2b019dc4214bb1592a
Evinpark1717 <60668200+Evinpark1717@users.noreply.github.com>
2020-02-04 13:44:48 -0500

    did a thing.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _317324Program2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lblGreeting.Content = "hello " + txtBName.Text + ". May the power of god rest within you";
+        }
+
+        private void txtBName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
